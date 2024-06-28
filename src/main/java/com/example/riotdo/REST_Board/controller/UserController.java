d41ee3c09a88b0138c6a1aca5efc4f002011ae96
snowykte0426 <snowykte0426@naver.com>
2024-06-28 23:19:05 +0900

    게시물 작성,조회,수정,삭제 구현

--- src/main/java/com/example/riotdo/REST_Board/controller/UserController.java
+++ src/main/java/com/example/riotdo/REST_Board/controller/UserController.java
@@ -3,31 +3,33 @@ package com.example.riotdo.REST_Board.controller;
 import com.example.riotdo.REST_Board.dto.RegisterDto;
 import com.example.riotdo.REST_Board.response.Response;
 import com.example.riotdo.REST_Board.service.UserService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "User Controller", description = "사용자 관리 API")
 public class UserController {
     private final UserService userService;
 
-    @ApiOperation(value = "전체 회원 보기", notes = "전체 회원 조회")
+    @Operation(summary = "전체 회원 보기", description = "전체 회원 조회")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/users")
     public Response<?> findAll() {
         return new Response<>("true", "조회성공", userService.findAll());
     }
 
-    @ApiOperation(value = "유저 찾기", notes = "개별 유저 조회")
+    @Operation(summary = "유저 찾기", description = "개별 유저 조회")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/users/{id}")
     public Response<?> findUser(@PathVariable("id") Integer id) {
-        return new Response<>("true", "조회성공", userService.findAll(id));
+        return new Response<>("true", "조회 성공", userService.findUser(id));
     }
 
-    @ApiOperation(value = "회원가입", notes = "회원가입 진행")
+    @Operation(summary = "회원가입", description = "회원가입 진행")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/auth")
     public Response<?> register(@RequestBody RegisterDto registerDto) {
--- src/main/java/com/example/riotdo/REST_Board/repository/UserRepository.java
+++ src/main/java/com/example/riotdo/REST_Board/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.riotdo.REST_Board.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
